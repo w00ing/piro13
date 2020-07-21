0074b3d0b727301b5f90edda5126730fa8c9fe11
w00ing <kimwy1997@gmail.com>
2020-07-21 10:50:07 +0900

    Upload data for 0718

---
+++ 0718_django/django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/koooo/dev/p-rogramming/piro13/0718_django/django/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
